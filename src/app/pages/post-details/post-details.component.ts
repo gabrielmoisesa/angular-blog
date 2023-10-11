a2ebb9aa43cb53dea42f380f48ab766d005fcffc
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-11 01:36:31 -0300

    feat: add post details initial html content

--- src/app/pages/post-details/post-details.component.ts
+++ src/app/pages/post-details/post-details.component.ts
@@ -5,28 +5,27 @@ import data from 'src/app/data';
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
-  styleUrls: ['./post-details.component.css']
+  styleUrls: ['./post-details.component.css'],
 })
 export class PostDetailsComponent {
   postTitle: string = '';
-
-  constructor(private route: ActivatedRoute) { }
   
+  constructor(private route: ActivatedRoute) {}
+
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.postTitle = params['title'];
     });
   }
 
   formatTitleForURL(title: string): string {
-    title = title.replace(/[^a-zA-Z0-9\s-]/g, ''); 
-    title = title.replace(/,/g, ''); 
-    title = title.toLowerCase(); 
-    return title.replace(/\s+/g, '-'); 
+    title = title.replace(/[^a-zA-Z0-9\s-]/g, '');
+    title = title.replace(/,/g, '');
+    title = title.toLowerCase();
+    return title.replace(/\s+/g, '-');
   }
 
   getPostDataByTitle(title: string) {
-    return data.find(post => this.formatTitleForURL(post.title) === title);
+    return data.find((post) => this.formatTitleForURL(post.title) === title);
   }
-
 }
