866b4a8f70eacfe48e8f6e4cd7ee2e590a7716ab
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-11 01:25:42 -0300

    feat: create post-details functions to get post data

--- src/app/pages/post-details/post-details.component.ts
+++ src/app/pages/post-details/post-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import data from 'src/app/data';
 
 @Component({
   selector: 'app-post-details',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-details.component.css']
 })
 export class PostDetailsComponent {
+  postTitle: string = '';
+
+  constructor(private route: ActivatedRoute) { }
+  
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.postTitle = params['title'];
+    });
+  }
+
+  formatTitleForURL(title: string): string {
+    title = title.replace(/[^a-zA-Z0-9\s-]/g, ''); 
+    title = title.replace(/,/g, ''); 
+    title = title.toLowerCase(); 
+    return title.replace(/\s+/g, '-'); 
+  }
+
+  getPostDataByTitle(title: string) {
+    return data.find(post => this.formatTitleForURL(post.title) === title);
+  }
 
 }
