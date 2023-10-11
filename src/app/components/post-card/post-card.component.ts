2c095da8ef0856e8dc802ec5e61a5ddfc6fd2642
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-11 00:52:47 -0300

    feat: fix title route link & create function to format title to a route type

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -6,8 +6,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./post-card.component.css']
 })
 export class PostCardComponent {
-
   @Input() imgSrc: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png';
   @Input() date: string = 'Post Date';
   @Input() title: string = 'Post Title';
+
+  formatTitleForURL(title: string): string {
+  title = title.replace(/[^a-zA-Z0-9\s-]/g, ''); // Removes anything that's not a letter, number, space, or hyphen
+  title = title.replace(/,/g, ''); // Remove commas
+  title = title.toLowerCase(); // Convert the title to lowercase
+  return title.replace(/\s+/g, '-'); // Replace spaces with hyphens
+  }
+
+  get postUrl(): string {
+    return this.formatTitleForURL(this.title);
+  }
 }
