192d175f43543f0c6b40e162b075430664a9612f
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-11 02:29:48 -0300

    feat: create color key to post data & use it in post details

--- src/app/data.ts
+++ src/app/data.ts
@@ -5,21 +5,25 @@ const data:PostType[] = [
     img: 'https://miro.medium.com/v2/resize:fit:720/0*1yo17Ov28sa7p33N',
     date: 'Sep 7',
     title: 'New free, interactive Angular course for beginners on Sololearn',
+    color: '#DD0031',
   },
   {
     img: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*CNy2EclZxC1hmi_govb_7w.png',
     date: 'May 3',
     title: 'Angular v16 is here!',
+    color: '#399AE9',
   },
   {
     img: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*BMDyhp4RU0SQuG5FSCG4MA.png',
     date: 'Apr 17',
     title: 'What’s next for Server Side Rendering in Angular',
+    color: '#FF2A4C',
   },
   {
     img: 'https://miro.medium.com/v2/resize:fit:720/0*ULvs_4zdRf8Fi8_4',
     date: 'Apr 29, 2022',
-    title: 'Angular at I/O 2022'
+    title: 'Angular at I/O 2022',
+    color: '#FBBC03',
   },
 ]
 
--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,5 @@ export type PostType = {
   img: string;
   date: string;
   title: string;
+  color?: string;
 }
