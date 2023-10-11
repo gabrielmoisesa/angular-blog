9e78eac0db47d0cdf85f62acfa04f16fa1fdb6d8
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-10 21:37:30 -0300

    feat: create post card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
