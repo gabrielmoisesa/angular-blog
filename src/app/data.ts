3d399f1e33e62f8f6b0e025e8f7a6bb6d7d0fb9c
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-23 20:54:11 -0300

    feat: create content item for posts, render them in html & heading and italic styles

--- src/app/data.ts
+++ src/app/data.ts
@@ -5,6 +5,49 @@ const data:PostType[] = [
     img: 'https://miro.medium.com/v2/resize:fit:720/0*1yo17Ov28sa7p33N',
     date: 'Sep 7',
     title: 'New free, interactive Angular course for beginners on Sololearn',
+    content: [
+      {
+        type: 'paragraph',
+        text: 'We are delighted to announce that we’ve collaborated with the online interactive code learning platform Sololearn to develop an introductory course to Angular. The course is available for free on Sololearn’s platform to anyone who wants to learn Angular.',
+      },
+      {
+        type: 'paragraph',
+        text: 'With hands-on coding exercises and projects, learners get practical experience working with real examples of Angular code. They’ll learn the fundamentals of Angular, such as components, routing, and more.',
+      },
+      {
+        type: 'paragraph',
+        text: 'Example of how you can use Sololearn’s interactive platform to learn Angular',
+        format: 'italic',
+      },
+      {
+        type: 'paragraph',
+        text: 'The course is designed to be accessible to new developers, while also being helpful for intermediate developers new to Angular who want to learn more Angular concepts and skills. Sololearn is offering both the Angular course for beginners, as well as a full Angular learning path for those completely new to coding, which brings them up to speed in HTML, CSS, and JavaScript too.',
+      },
+      {
+        type: 'heading',
+        text: 'Meet Sololearn',
+      },
+      {
+        type: 'paragraph',
+        text: 'Sololearn is an innovative and interactive edtech platform that has helped over 30 million people in every country around the world learn to code. It offers bite-sized lessons that accommodate different learner schedules and courses on a variety of programming languages, including Python, Java, JavaScript, and C++. The courses are designed for beginners and experienced developers alike while covering topics from the basics of programming to more advanced concepts. Sololearn offers a variety of tools and resources to help learners practice their coding skills — including AI tools, a code editor, debugger, and the support of a community forum of over 30 million learners.',
+      },
+      {
+        type: 'heading',
+        text: 'Building Great Apps',
+      },
+      {
+        type: 'paragraph',
+        text: 'We continue to enable developers to learn Angular and build scalable web applications with confidence. This collaboration with Sololearn allows you to learn new Angular concepts at your own pace, from your phone, wherever you are.',
+      },
+      {
+        type: 'paragraph',
+        text: 'The free Angular course on Sololearn is a great resource for training new Angular developers.',
+      },
+      {
+        type: 'paragraph',
+        text: 'Let us know what you think of it.',
+      },
+    ],
   },
   {
     img: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*CNy2EclZxC1hmi_govb_7w.png',
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,13 @@
+export type ContentItem = {
+  type: 'heading' | 'paragraph';
+  text: string;
+  format?: 'italic';
+}
+
 export type PostType = {
   img: string;
   date: string;
   title: string;
   color?: string;
+  content?: ContentItem[];
 }
